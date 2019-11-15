121d1b27336f4ac5fdd027e63cce380a770b3a0c
Joost Kager <joost.kager@virtualsciences.nl>
2019-11-15 14:58:44 +0100

    request to ACE login API

--- src/main/java/team2/sofa/sofa/controller/LoginController.java
+++ src/main/java/team2/sofa/sofa/controller/LoginController.java
@@ -1,12 +1,30 @@
 package team2.sofa.sofa.controller;
 
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+import com.google.gson.Gson;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import team2.sofa.sofa.model.Client;
 import team2.sofa.sofa.model.Employee;
@@ -24,6 +42,12 @@ public class LoginController {
     PasswordValidator passwordValidator;
     @Autowired
     ClientViewController clientViewController;
+
+    private final RestTemplate restTemplate;
+
+    public LoginController(RestTemplateBuilder restTemplateBuilder) {
+        this.restTemplate = restTemplateBuilder.build();
+    }
 
     @GetMapping(value = "login")
     public String indexHandler(Model model) {
@@ -47,6 +71,8 @@ public class LoginController {
 
     @PostMapping(value = "loginClientHandler")
     public String loginClientHandler(Client client, Model model, RedirectAttributes redirectAttributes) {
+        Client c = createPost(client);
+        System.out.println(c);
         boolean loginOk = passwordValidator.validateClientPassword(client);
         if (loginOk) {
             model.addAttribute("sessionclient", new Client());
@@ -80,4 +106,34 @@ public class LoginController {
             return "login_employee";
         }
     }
+
+    public Client createPost(Client client) {
+        String url = "http://host.docker.internal:7800/loginapi/v1/login";
+    
+        // create headers
+        HttpHeaders headers = new HttpHeaders();
+        // set `content-type` header
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        // set `accept` header
+        headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
+    
+        // create a map for post parameters
+        Map<String, Object> map = new HashMap<>();
+        map.put("username", client.getUsername());
+        map.put("password", client.getPassword());
+    
+        // build the request
+        HttpEntity<Map<String, Object>> entity = new HttpEntity<>(map, headers);
+    
+        // send POST request
+        ResponseEntity<Client> response = this.restTemplate.postForEntity(url, entity, Client.class);
+    
+        // check response status code
+        if (response.getStatusCode() == HttpStatus.CREATED) {
+            return response.getBody();
+        } else {
+            return null;
+        }
+    }    
 }
+
